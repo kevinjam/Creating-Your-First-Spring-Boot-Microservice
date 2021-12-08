18407aa4e125549d76a114fdce2ec8b33c11ab20
kevinjam <kevin.janvier5@gmail.com>
2021-12-09 01:15:57 +0300

    feat : add rest api
    adding rest controller to get api

---
+++ src/main/java/com/thinkdevs/ec/repo/TourRatingRepository.java
@@ -0,0 +1,30 @@
+package com.thinkdevs.ec.repo;
+
+
+import com.thinkdevs.ec.domain.TourRating;
+import com.thinkdevs.ec.domain.TourRatingPk;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+import java.util.Optional;
+
+@RepositoryRestResource(exported = false)
+public interface TourRatingRepository extends CrudRepository<TourRating, TourRatingPk> {
+
+    /**
+     * Lookup all the TourRatings for a tour.
+     *
+     * @param tourId is the tour Identifier
+     * @return a List of any found TourRatings
+     */
+    List<TourRating> findByPkTourId(Integer tourId);
+
+    /**
+     * Lookup a TourRating by the TourId and Customer Id
+     * @param tourId tour identifier
+     * @param customerId customer identifier
+     * @return Optional of found TourRatings.
+     */
+    Optional<TourRating> findByPkTourIdAndPkCustomerId(Integer tourId, Integer customerId);
+}
