a288d893feeb6d173362ba0c2034e1d68fec9cc4
kevinjam <kevin.janvier5@gmail.com>
2021-12-09 00:27:13 +0300

    feat: add sample data

--- src/main/java/com/thinkdevs/ec/repo/TourPackageRepository.java
+++ src/main/java/com/thinkdevs/ec/repo/TourPackageRepository.java
@@ -3,6 +3,10 @@ package com.thinkdevs.ec.repo;
 import com.thinkdevs.ec.domain.TourPackage;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface TourPackageRepository extends CrudRepository<TourPackage, String> {
+
+    Optional<TourPackage> findByName(String name);
 }
 
